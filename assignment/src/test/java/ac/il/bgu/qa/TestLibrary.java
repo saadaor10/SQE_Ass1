7f158f659ffe66456267a17473564a58179c788a
orsaada <saadao@post.bgu.ac.il>
2024-01-17 22:46:56 +0200

    new project

---
+++ assignment/src/test/java/ac/il/bgu/qa/TestLibrary.java
@@ -0,0 +1,10 @@
+package ac.il.bgu.qa;
+
+import ac.il.bgu.qa.errors.*;
+import ac.il.bgu.qa.services.*;;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.*;
+import org.mockito.*;
+
+public class TestLibrary {
+}
