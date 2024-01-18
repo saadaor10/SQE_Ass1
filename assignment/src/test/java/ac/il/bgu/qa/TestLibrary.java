8a3133c18009561fdb6e30614930a0ae7c1bac7a
orsaada <saadao@post.bgu.ac.il>
2024-01-18 19:31:19 +0200

    register user that already exist

--- assignment/src/test/java/ac/il/bgu/qa/TestLibrary.java
+++ assignment/src/test/java/ac/il/bgu/qa/TestLibrary.java
@@ -1,12 +1,14 @@
 package ac.il.bgu.qa;
 
+import ac.il.bgu.qa.errors.*;
 import ac.il.bgu.qa.services.*;;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.*;
+import org.junit.jupiter.api.function.Executable;
 import org.mockito.*;
 import org.mockito.junit.jupiter.*;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -19,8 +21,9 @@ public class TestLibrary {
     ReviewService mockReviewService;
 
     @Mock
+    NotificationService mockNotificationService;
+    @Mock
     Library mockLibrary;
-
 
 
     @BeforeEach
@@ -30,7 +33,7 @@ public class TestLibrary {
     }
 
     @Test
-    public void registerSuccessfullyUser_whenGetUser() {
+    public void borrowBookSuccessfully_whenGetBookISBMAndUserID() {
         // 1. Arrange
         Book book = new Book("978-3-16-148410-0", "Test Book", "Test Author");
         String userId = "123456789012";
@@ -39,8 +42,6 @@ public class TestLibrary {
         // 1.2. Stubbing - Define behavior for mockApiClient
         when(mockDatabaseService.getBookByISBN(book.getISBN())).thenReturn(book);
         when(mockDatabaseService.getUserById(userId)).thenReturn(new User("John Doe", userId, mock(NotificationService.class)));
-//
-//        // Act
 
         // 2. Action
         // 2.1. Call the method under test
@@ -55,4 +56,24 @@ public class TestLibrary {
         // 3.2. Assert the result
         assertTrue(book.isBorrowed());
     }
+
+    @Test
+    void registerUser_whenUserAlreadyExists() {
+        // Arrange
+        String userId = "123456789015";
+        User existingUser = new User("John Doe", userId, mock(NotificationService.class));
+
+        // Stubbing - Define behavior for mockDatabaseService
+        when(mockDatabaseService.getUserById(userId)).thenReturn(existingUser);
+
+        // Act and Assert
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> mockLibrary.registerUser(existingUser));
+
+        // Verify interactions
+        verify(mockDatabaseService, times(1)).getUserById(userId);
+
+        // Assert the result
+        assertEquals("User already exists.", exception.getMessage());
+    }
+
 }
